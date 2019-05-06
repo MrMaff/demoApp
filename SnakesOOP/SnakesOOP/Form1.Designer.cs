d20cd60bc7ef038ec87178845fa93bd0d1a72adf
Mr Maff <mafflong@online.sch.im>
2019-05-06 17:02:38 +0100

    Created initial launch form

---
+++ SnakesOOP/SnakesOOP/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace SnakesOOP
+{
+    partial class frm_Launcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_NewGame = new System.Windows.Forms.Button();
+            this.btn_Quit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_NewGame
+            // 
+            this.btn_NewGame.Location = new System.Drawing.Point(131, 190);
+            this.btn_NewGame.Name = "btn_NewGame";
+            this.btn_NewGame.Size = new System.Drawing.Size(172, 58);
+            this.btn_NewGame.TabIndex = 0;
+            this.btn_NewGame.Text = "New Game";
+            this.btn_NewGame.UseVisualStyleBackColor = true;
+            // 
+            // btn_Quit
+            // 
+            this.btn_Quit.Location = new System.Drawing.Point(131, 285);
+            this.btn_Quit.Name = "btn_Quit";
+            this.btn_Quit.Size = new System.Drawing.Size(172, 61);
+            this.btn_Quit.TabIndex = 1;
+            this.btn_Quit.Text = "Quit Application";
+            this.btn_Quit.UseVisualStyleBackColor = true;
+            this.btn_Quit.Click += new System.EventHandler(this.btn_Quit_Click);
+            // 
+            // frm_Launcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 572);
+            this.Controls.Add(this.btn_Quit);
+            this.Controls.Add(this.btn_NewGame);
+            this.Name = "frm_Launcher";
+            this.Text = "Snakes and Ladders";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_NewGame;
+        private System.Windows.Forms.Button btn_Quit;
+    }
+}
+
---
+++ SnakesOOP/SnakesOOP/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakesOOP
+{
+    public partial class frm_Launcher : Form
+    {
+        public frm_Launcher()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Quit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
