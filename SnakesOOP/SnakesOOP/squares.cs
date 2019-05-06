c111ab93cdb72109fd05d18f36f4f4ebd6d8711a
Mr Maff <mafflong@online.sch.im>
2019-05-06 21:55:04 +0100

    Added Square Classes

---
+++ SnakesOOP/SnakesOOP/squares.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakesOOP
+{
+    public abstract class Square
+    {
+        protected int number;
+        protected string type;
+        protected int action;
+        protected List<Player> occupiers = new List<Player>();
+
+        public int Number { get; }
+
+        public Square()
+        { }
+
+        public Square(int number, int action)
+        {
+            this.number = number;
+            this.action = action;
+        }
+
+        public abstract void Action();
+
+        public void AddPlayer(Player currentPlayer)
+        {
+            currentPlayer.Location = this.number;
+            occupiers.Add(currentPlayer);
+        }
+
+        public void RemovePlayer(Player currentPlayer)
+        {
+            occupiers.Remove(currentPlayer);
+        }
+
+    }
+
+    public class Normal : Square
+    {
+        public Normal()
+        {
+            this.type = "N";
+        }
+
+        public Normal(int number, int action)
+            : base(number, action)
+        {
+            this.type = "N";
+        }
+
+        public override void Action()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+
+    public class Snake : Square
+    {
+        public Snake()
+        {
+            this.type = "S";
+        }
+
+        public Snake(int number, int action)
+            : base(number, action)
+        {
+            this.type = "S";
+        }
+
+        public override void Action()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+
+    public class Ladder : Square
+    {
+        public Ladder()
+        {
+            this.type = "L";
+        }
+
+        public Ladder(int number, int action)
+            : base(number, action)
+        {
+            this.type = "L";
+        }
+
+        public override void Action()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+
+    public class Final : Square
+    {
+        public Final()
+        {
+            this.type = "F";
+        }
+
+        public Final(int number, int action)
+            : base(number, action)
+        {
+            this.type = "F";
+        }
+
+        public override void Action()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
