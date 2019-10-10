55f45cf9f52c7dc0e6308485e6bc0289abb2edba
MrMaff <mafflong@online.sch.im>
2019-10-10 16:40:09 +0100

    start up

---
+++ QuizNight/QuizNight/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizNight
+{
+    class Program
+    {
+        static string name;
+        static int score;
+
+        static void Main(string[] args)
+        {
+            DisplayQuizName();
+            GetUserName();
+            PlayQuiz();
+            DisplayScore();
+        }
+
+        static void DisplayQuizName()
+        {
+            Console.Clear();
+            Console.WriteLine("Quiz Night!");
+            Console.WriteLine("============/n");
+        }
+
+        static void GetUserName()
+        {
+            Console.WriteLine("Player, please enter your name...");
+            Console.Write("Name: ");
+            name = Console.ReadLine();
+        }
+
+        static void PlayQuiz()
+        {
+
+        }
+
+        static void DisplayScore()
+        {
+
+        }
+    }
+}
