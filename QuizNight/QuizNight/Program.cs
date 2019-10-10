9bc1ec9942d8b3253d0be00d8e6cac17406cb336
Mr Maff <mafflong@online.sch.im>
2019-10-10 20:50:13 +0100

    Finished app

--- QuizNight/QuizNight/Program.cs
+++ QuizNight/QuizNight/Program.cs
@@ -17,13 +17,14 @@ namespace QuizNight
             GetUserName();
             PlayQuiz();
             DisplayScore();
+            AnyKey();
         }
 
         static void DisplayQuizName()
         {
             Console.Clear();
             Console.WriteLine("Quiz Night!");
-            Console.WriteLine("============/n");
+            Console.WriteLine("============\n");
         }
 
         static void GetUserName()
@@ -44,28 +45,47 @@ namespace QuizNight
                 {"Where in memory are variables declared in subroutines?","The stack" },
                 {"What is it called when a frame is added to the stack?","pushing" },
                 {"what is it called when a fram is removed from the stack?","popping" },
-                {"","" },
-                {"","" },
-                {"","" }
+                {"When using parameters in procedures, variables are passed by....","value" },
+                {"When using parameters in procedures, objects and arrays are passed by...","reference" },
+                {"Which type of subroutine always returns a value?","function" }
             };
 
 
             for (int i = 0; i < 10; i++)
             {
                 DisplayQuizName();
-                AskQuestion();
+                AskQuestion(quiz[i,0],quiz[i,1]);
             }
 
         }
 
         static void DisplayScore()
         {
-            Console.WriteLine("/nThe score is : {0}/n", score);
+            Console.WriteLine("\nThe score is : {0}\n", score);
         }
 
         static void AskQuestion(string question, string answer)
         {
+            string userAnswer;
 
+            Console.WriteLine(question);
+            userAnswer = Console.ReadLine();
+            if (userAnswer.ToLower() == answer.ToLower())
+            {
+                Console.WriteLine("Correct!");
+                score++;
+            }
+            else
+            {
+                Console.WriteLine("Sorry.\nThe answer is: {0}\n", answer);
+            }
+            AnyKey();
+        }
+
+        static void AnyKey()
+        {
+            Console.WriteLine("\nPress any key to continue...\n");
+            Console.ReadKey();
         }
     }
 }
