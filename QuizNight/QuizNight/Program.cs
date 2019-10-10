a81c90ccd8d9a6596b80c2852314aa9402750e68
Mr Maff <mafflong@online.sch.im>
2019-10-10 20:06:22 +0100

    Added some questions. More to follow.

--- QuizNight/QuizNight/Program.cs
+++ QuizNight/QuizNight/Program.cs
@@ -35,10 +35,35 @@ namespace QuizNight
 
         static void PlayQuiz()
         {
+            string[,] quiz = {
+
+                {"What is 15.75 in fixed point binary?","11111100" },
+                {"What is the smallest number in 8 bit two's complement?","-128" },
+                {"What is the biggest number in 4 bit two's complement?","7" },
+                {"Where in memory are arrays declared?","The heap" },
+                {"Where in memory are variables declared in subroutines?","The stack" },
+                {"What is it called when a frame is added to the stack?","pushing" },
+                {"what is it called when a fram is removed from the stack?","popping" },
+                {"","" },
+                {"","" },
+                {"","" }
+            };
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                DisplayQuizName();
+                AskQuestion();
+            }
 
         }
 
         static void DisplayScore()
+        {
+            Console.WriteLine("/nThe score is : {0}/n", score);
+        }
+
+        static void AskQuestion(string question, string answer)
         {
 
         }
