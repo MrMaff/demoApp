5e9ef6bf14099d05a45b2a44e0caafa57fd145d1
MrMaff <mafflong@online.sch.im>
2019-12-17 14:10:20 +0000

    wordcount

---
+++ DemoHashSet/DemoHashSet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoHashSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> somenumbers = new HashSet<int>() { 1, 2, 3, 4, 5, 6 };
+
+            Console.WriteLine(somenumbers.Count);
+            Console.WriteLine("Adding the number 12!");
+            somenumbers.Add(12);
+            Console.WriteLine(somenumbers.Count);
+            Console.WriteLine("Adding the numbers 4 and 5!");
+            somenumbers.Add(4);
+            somenumbers.Add(5);
+            Console.WriteLine("Adding the numbers -1");
+            somenumbers.Add(-1);
+            Console.WriteLine(somenumbers.Count);
+            Console.WriteLine("Outputting contents of hashset....");
+            foreach (int number in somenumbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ dictionaryWordCount/dictionaryWordCount/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionaryWordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, int> words = new Dictionary<string, int>();
+            string sentance = "Hello, this is me saying hello. I hope you are well. It is quite important that you do feel well, at least that's what I think.".ToLower();
+            string[] seperators = { ". ", ", ", " ", "! " };
+            string[] wordArray = sentance.Split(seperators, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var word in wordArray)
+            {
+                
+                if (words.ContainsKey(word))
+                {
+                    words[word] = words[word] + 1;
+                }
+                else
+                {
+                    words.Add(word, 1);
+                }
+            }
+
+
+            var sorted = words.OrderBy(temp =>temp.Value);
+
+            foreach (KeyValuePair<string, int> word in sorted)
+            {
+                Console.WriteLine(word.Key + " " + word.Value);
+            }
+
+            //foreach (KeyValuePair<string, int> definition in words)
+            //{
+            //    Console.WriteLine(definition.Key + " " + definition.Value);
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
