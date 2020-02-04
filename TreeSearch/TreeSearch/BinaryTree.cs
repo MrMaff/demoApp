09b2340d53a7e5d22d191482bb985af4780749fc
Mr Maff <mafflong@online.sch.im>
2020-02-04 00:47:23 +0000

    Created Binary Tree Search

---
+++ TreeSearch/TreeSearch/BinaryTree.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeSearch
+{
+
+    class TreeNode
+    {
+        public int Number { get; }
+        public TreeNode left = null;
+        public TreeNode right = null;
+
+        public TreeNode(int number)
+        {
+            this.Number = number;
+        }
+
+        public void Add(TreeNode NewLeaf)
+        {
+            if (NewLeaf.Number < this.Number)
+            {
+                if (this.left == null)
+                {
+                    this.left = NewLeaf;
+                }
+                else
+                {
+                    this.left.Add(NewLeaf);
+                }
+            }
+            if (NewLeaf.Number > this.Number)
+            {
+                if (this.right == null)
+                {
+                    this.right = NewLeaf;
+                }
+                else
+                {
+                    this.right.Add(NewLeaf);
+                }
+            }
+        }
+
+        public bool Contains(int num)
+        {
+            bool present = true;
+            if (this.Number == num)
+            {
+                present = true;
+            }
+            else if((this.left == null || this.left.Contains(num) == false) && (this.right == null || this.right.Contains(num)==false))
+            {
+                present = false;
+            }
+
+            return present;
+        }
+    }
+
+    class BinaryTree
+    {
+        private TreeNode rootNode = null;
+
+        public BinaryTree()
+        { }
+
+
+        public BinaryTree(int[] numberData, bool sorted)
+        {
+            if (sorted == false)
+            {
+                Array.Sort(numberData);
+            }
+
+            int mid = numberData.Length / 2;
+            this.rootNode = new TreeNode(numberData[mid]);
+            //Add to the right
+            AddMid(numberData, mid + 1, numberData.Length-1);
+            //Add to the Left
+            AddMid(numberData, 0, mid - 1);
+        }
+
+        private void AddMid(int[] numberData, int lpointer, int rpointer)
+        {
+            int mid = (lpointer + rpointer) / 2;
+            this.rootNode.Add(new TreeNode(numberData[mid]));
+            if (lpointer < rpointer)
+            {
+                
+                //Add to the right
+                AddMid(numberData, mid + 1, rpointer);
+                //Add to the Left
+                AddMid(numberData, lpointer, mid - 1);
+            }
+            
+
+        }
+
+       public bool Contains(int num)
+        {
+            return this.rootNode.Contains(num);
+        }
+    }
+
+    
+}
---
+++ TreeSearch/TreeSearch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                int[] NumberArray = { 77, 5, 3, 78, 9, 9, 9, 90, 34, 36, 500, 79, 1, 7, 66, 21, 92, 455, 46 };
+                int searchNo;
+
+                Console.Write("What number do you want to find: ");
+                int.TryParse(Console.ReadLine(), out searchNo);
+
+                BinaryTree myTree = new BinaryTree(NumberArray, false);
+
+                Console.WriteLine(myTree.Contains(searchNo));
+                Console.ReadKey();
+            
+
+
+        }
+    }
+}
