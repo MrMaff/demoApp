6d2d97519367a8e32feeed1a9d2f32cc37558f60
Mr Maff <mafflong@online.sch.im>
2020-03-09 23:35:29 +0000

    done

---
+++ FebTestSectB/FebTestSectB/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FebTestSectB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Prime;
+            int Count1;
+            int Count2;
+            Console.WriteLine("The first few prime numbers are:");
+            for (Count1 = 2; Count1 <= 50; Count1++)
+            {
+                Count2 = 2;
+                Prime = "Yes";
+                while (Count2 * Count2 <= Count1)
+                {
+                    if (Count1 % Count2 == 0)
+                    {
+                        Prime = "No";
+                    }
+                    Count2 = Count2 + 1;
+                }
+
+                if (Prime == "Yes")
+                {
+                    Console.WriteLine(Count1);
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
